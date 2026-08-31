85fa63bed2f7f5e6040025326484208cf973befd
Acrimon <jwejd@icloud.com>
2026-08-31 21:24:22 +0200

    add empty sync modules

--- scr/src/sync/mod.rs
+++ scr/src/sync/mod.rs
@@ -1,1 +1,17 @@
+mod notify; // TODO
 
+mod ll_semaphore; // TODO
+mod semaphore; // TODO
+
+mod oneshot; // TODO
+mod mpmc; // TODO
+mod broadcast; // TODO
+mod watch; // TODO
+
+mod once_cell; // TODO
+mod lazy_cell; // TODO
+
+//mod barrier;
+
+//mod mutex;
+//mod rwlock;
